7ffa9fb357ee175df1ac2a2a89e0d10348508f1d
yohan <rmvmf96@gmail.com>
2024-09-25 21:40:52 +0900

    프로젝트 생성 :  gradle 선택,  jar 패키징 선택

---
+++ Api/yh/momo/src/test/java/com/toyproject/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
