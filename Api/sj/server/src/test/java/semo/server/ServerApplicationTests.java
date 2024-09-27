f8d70483d7b7924ee87fadaac04d85f616cffe2d
Kimseungin0529 <whffkaos007@naver.com>
2024-09-27 09:40:43 +0900

    chore : 초기 파일 설정

---
+++ Api/sj/server/src/test/java/semo/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package semo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
